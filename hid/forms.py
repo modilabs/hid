a84133d8c3693df29ab9bcf7744bbbd1ee034d96
katembu <mose.kate@gmail.com>
2012-11-06 17:50:54 +0300

    Add select mvp-site form

--- hid/forms.py
+++ hid/forms.py
@@ -2,6 +2,9 @@
 # maintainer: katembu
 
 from django.forms import ModelForm
+from django import forms
+from django.forms.widgets import CheckboxInput
+from django.contrib.auth.models import User
 from hid.models import *
 
 
@@ -9,3 +12,17 @@ class IdentifierForm(ModelForm):
 
     class Meta:
         model = IdentifierRequest
+
+
+class SiteChoice(forms.Form):
+    for_site = forms.ChoiceField(widget=forms.Select,
+                                 label="Select Site",
+                                 required=True)
+
+    def __init__(self, username):
+        self.username = username
+        super(SiteChoice, self).__init__()
+        user = User.objects.get(username=username)
+        choices = [(u.site, u.site.name) for u in SitesUser.objects
+                   .filter(user=user)]
+        self.fields['for_site'].choices = choices
