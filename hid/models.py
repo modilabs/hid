45386bbcf85cc25b69dbf6ce5de39d0f4aad0749
katembu <mose.kate@gmail.com>
2012-12-11 12:08:48 +0300

    remove utils import from models

--- hid/models.py
+++ hid/models.py
@@ -6,7 +6,6 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _, ugettext
 from django.db.models.signals import post_save, pre_save
-from hid.utils import *
 
 import reversion
 
