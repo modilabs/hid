9a1dc1ba688f3a307af1b180c8738a5d1fc670aa
katembu <mose.kate@gmail.com>
2013-02-27 10:04:32 +0300

    add cases model to admin

--- hid/admin.py
+++ hid/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 
 from hid.models import Site, Identifier, IdentifierRequest, SitesUser, \
-    IssuedIdentifier
+    IssuedIdentifier, Cases
 
 
 def site(obj):
@@ -20,6 +20,7 @@ class IssuedIdentifierAdmin(admin.ModelAdmin):
 
 admin.site.register(IssuedIdentifier, IssuedIdentifierAdmin)
 admin.site.register(Site)
+admin.site.register(Cases)
 admin.site.register(Identifier)
 admin.site.register(IdentifierRequest)
 admin.site.register(SitesUser)
